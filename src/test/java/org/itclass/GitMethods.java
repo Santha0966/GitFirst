07d38c70ff7c1b68b2c1281a4778af87e38dfe8d
Santha0966 <rsanthadevi1966@gmail.com>
2022-04-05 16:15:08 +0530

    Removed few Methods in Class1

--- src/test/java/org/itclass/GitMethods.java
+++ src/test/java/org/itclass/GitMethods.java
@@ -9,20 +9,4 @@ public class GitMethods {
 		System.out.println("Method 2");
 
 	}
-	public void method3() {
-		System.out.println("Method 3");
-
 	}
-	public void method4() {
-		System.out.println("Method 4");
-
-	}
-	public void method5() {
-		System.out.println("Method 4");
-
-	}
-	public void method6() {
-		System.out.println("Method 4");
-
-	}
-}
