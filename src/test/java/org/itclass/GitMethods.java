a9699dd171f1d977f072db3455767a3c029aa776
Santha0966 <rsanthadevi1966@gmail.com>
2022-04-05 14:35:33 +0530

    Added Class1

---
+++ src/test/java/org/itclass/GitMethods.java
@@ -0,0 +1,20 @@
+package org.itclass;
+
+public class GitMethods {
+	public void method1() {
+		System.out.println("Method 1");
+
+	}
+	public void method2() {
+		System.out.println("Method 2");
+
+	}
+	public void method3() {
+		System.out.println("Method 3");
+
+	}
+	public void method4() {
+		System.out.println("Method 4");
+
+	}
+}
